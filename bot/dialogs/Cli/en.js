055d05ad1932acadc6e1a05f08e5c0590a100a93
Francescosandri84 <33425323+Francescosandri84@users.noreply.github.com>
2018-06-21 12:48:10 +0200

    - %0A per line feed in Telegram (tentativo)

--- bot/dialogs/Cli/en.js
+++ bot/dialogs/Cli/en.js
@@ -7,7 +7,7 @@ var lang_en = {
     cliWelcome: "This is the bot CLI. Type 'commands' to see a list of commands, 'exit' to go back to the bot.",
     cliReady: "You are still in the CLI. Type 'commands' to see a list of commands, 'exit' to go back to the bot.",
     syntaxError: "Command not recognized or wrong command syntax. Type 'commands' to see a list of commands.",
-    endCommands: "This is a list of available commands:\ndeploy < framework(BW6 | TCI) > <applicationName> <environment(QAS|PROD)> <sandbox(if TCI)> <version(if PROD)> <ADlogin> <ADpassword>\nexamples:\ndeploy BW6 AppToDeploy QAS 99009900 Pswd-9900\ndeploy TCI AppToDeploy PROD ProdPublicSandbox 1.0.0 99009900 Pswd-9900",
+    endCommands: "This is a list of available commands:%0Adeploy < framework(BW6 | TCI) > <applicationName> <environment(QAS|PROD)> <sandbox(if TCI)> <version(if PROD)> <ADlogin> <ADpassword>%0Aexamples:%0Adeploy BW6 AppToDeploy QAS 99009900 Pswd-9900%0Adeploy TCI AppToDeploy PROD ProdPublicSandbox 1.0.0 99009900 Pswd-9900",
     wrongAppName: "There is no %s Application \"*%s*\" on GitHub. Please check the command syntax and retry.",
     confirmDeploy: "The deploy is being started. \n\n ..Please wait a few seconds..",
 errorMessage: "Oooops... something went wrong. Please don't panic and contact the support before retrying. Error code: \"*%s*\"",
